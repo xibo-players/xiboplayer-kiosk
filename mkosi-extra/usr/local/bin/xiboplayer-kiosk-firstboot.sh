20cff2c39a4b081c7ddc6c19f8a4205b39cf6676
Pau Aliagas <linuxnow@gmail.com>
2026-04-07 11:14:12 +0000

    fix: add autostart to firstboot, gnome-control-center + vendor.conf to atomic image

--- mkosi-extra/usr/local/bin/xiboplayer-kiosk-firstboot.sh
+++ mkosi-extra/usr/local/bin/xiboplayer-kiosk-firstboot.sh
@@ -13,5 +13,11 @@ MARKER="/var/lib/xiboplayer-kiosk-firstboot-done"
 echo "xibo:xibo" | chpasswd
 loginctl enable-linger xibo 2>/dev/null || true
 
+# Install setup wizard autostart for first boot (normal GNOME session)
+AUTOSTART_DIR="/home/xibo/.config/autostart"
+mkdir -p "$AUTOSTART_DIR"
+cp /usr/share/xiboplayer-kiosk/xiboplayer-setup.desktop "$AUTOSTART_DIR/"
+chown -R xibo:xibo /home/xibo/.config
+
 touch "$MARKER"
 echo "Xibo Kiosk first-boot complete"
